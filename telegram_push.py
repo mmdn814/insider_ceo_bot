fe72939e9c53cc4ed9b064bdaea8edfc127f979e
mmdn814 <mmdn814@gmail.com>
2025-06-11 13:58:58 +0800

    Create telegram_push.py

---
+++ telegram_push.py
@@ -0,0 +1,13 @@
+import requests
+from config import TELEGRAM_TOKEN, TELEGRAM_CHAT_ID
+
+def send_telegram_message(message):
+    url = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage"
+    data = {
+        "chat_id": TELEGRAM_CHAT_ID,
+        "text": message,
+        "parse_mode": "Markdown"
+    }
+    response = requests.post(url, data=data)
+    print("Telegram response:", response.status_code, response.text)
+    return response.json()
